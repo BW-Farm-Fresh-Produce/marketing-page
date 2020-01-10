e80b29157ebe950d1461d7b044b41efe31bce18d
Abdulhamid Azizy <55733008+HamidAzizy@users.noreply.github.com>
2020-01-09 18:34:10 -0800

    Updated Responsive Design.

--- slideshow.js
+++ slideshow.js
@@ -1,12 +1,10 @@
 var slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
@@ -16,7 +14,7 @@ function showSlides(n) {
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n < 1) {slideIndex = slides.length} ;
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
@@ -25,23 +23,5 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
-}
-var slideIndex = 0;
-showSlides();
-
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
   // setTimeout(showSlides, 4000); // Change image every 4 seconds
 }
-
-//Scrop up
-
-//Get the button
